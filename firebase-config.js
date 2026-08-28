baccf6b88ca4681f5e76c302c3c596009f2ab410
nawabtaibajani-cmyk <nawabtaibajani@gmail.com>
2026-08-28 18:02:48 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDLol8HV5x8ABgYoTXqIOVQ0u7QAArZ4ng",
+  authDomain: "nj72-39e55.firebaseapp.com",
+  projectId: "nj72-39e55",
+  storageBucket: "nj72-39e55.firebasestorage.app",
+  messagingSenderId: "685443749226",
+  appId: "1:685443749226:web:32ff25ba966664bdbe4113",
+  measurementId: "G-85ZWHSEKKD"
+};
